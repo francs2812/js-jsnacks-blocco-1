9557fe0d23979956223ad47c05e5809a5509872d
EmilioFrangella <emiliofrangella2812@gmail.com>
2021-05-04 16:21:14 +0200

    prova 2

--- js/script.js
+++ js/script.js
@@ -6,31 +6,26 @@ function somma(nom, cogn) {
 
 var nomi = ["Giulio", "Fabio"];
 var cognomi = ["Marzullotti", "Spaghettani", "Luciani", "Frangella", "Frattani"];
-
-//     for(var i = 0 ; i < cognomi.length; i++){
-//         somma(nomi[i], cognomi[i]);
-//         document.write(nomeCognome);
-//         console.log(nomi[i]);
-//     }
-// }
-// for(var i=0; i < nomi.length; i++){
-//     for(var i = 0 ; i < cognomi.length; i++){
-//                 somma(nomi[i], cognomi[i]);
-//                 document.write(nomeCognome+ " ");
-//                 console.log(nomeCognome);
-//             }
-// }
 var listaInvitati = "";
 
-for(var i = 0 ; i < cognomi.length; i++){
-    somma(nomi[0], cognomi[i]);
-    listaInvitati += "<li>"+ nomeCognome +"</li>";
-    console.log(nomeCognome);
-}
-for(var i = 0 ; i < cognomi.length; i++){
-    somma(nomi[1], cognomi[i]);
-    listaInvitati += "<li>"+ nomeCognome +"</li>";
-    console.log(nomeCognome);
-}
+
+for(var i=0; i < nomi.length; i++){
+    for(var i2 = 0 ; i2 < cognomi.length; i2++){
+                somma(nomi[i2], cognomi[i2]);
+                listaInvitati += "<li>"+ nomeCognome +"</li>";
+                console.log(nomeCognome);
+            }
+        }
+
+// for(var i = 0 ; i < cognomi.length; i++){
+//     somma(nomi[0], cognomi[i]);
+//     listaInvitati += "<li>"+ nomeCognome +"</li>";
+//     console.log(nomeCognome);
+// }
+// for(var i = 0 ; i < cognomi.length; i++){
+//     somma(nomi[1], cognomi[i]);
+//     listaInvitati += "<li>"+ nomeCognome +"</li>";
+//     console.log(nomeCognome);
+//}
 
 document.getElementById("lista").innerHTML = listaInvitati; 
